c7ae381564034f83a02a2d9c87b79e757d34a626
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-12 21:13:56 +0500

    creat type

--- src/ru/geekbrains/lesson_1/Main.java
+++ src/ru/geekbrains/lesson_1/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4L;
+        float e = 5.0f;
+        double f = 6.7;
+        boolean j = false;
+        char h = 'Z';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(j);
+        System.out.println(h);
     }
 }
